0264f75429c83d28984807df557e6e3ed96f014c
aakhmedkhodzhaev <teenager_08@inbox.ru>
2019-12-23 23:13:19 +0500

    3.1.1. update Пин-понг.[#209934]

--- chapter_001/src/main/java/ru/job4j/pingpong/RectangleMove.java
+++ chapter_001/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -3,36 +3,30 @@ package ru.job4j.pingpong;
 import javafx.scene.shape.Rectangle;
 
 public class RectangleMove implements Runnable {
+    // Квадратик
     private final Rectangle rect;
-    int x = 300;
+    // V = 1 pix/ms, скорость перемещения
+    int i = 1;
+
     public RectangleMove(Rectangle rect) {
         this.rect = rect;
     }
 
     @Override
     public void run() {
-        while(true) { // for(;;) // ещё один вариант
-
-            for (int i = 0; i < x; i++) {
-                this.rect.setX(i);
-                //  this.rect.setY(i); // если нужно с y
-
-                try {
-                    Thread.sleep(38);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        while (true) {
+            // выбор направления перемещения
+            this.rect.setX(this.rect.getX() + this.i);
+            // ограничения значения направления по площади
+            if(this.rect.getX()>300||this.rect.getX()<0)
+            {
+                this.i=-this.i;
             }
-            for (int i = x; i > 0; i--) {
-                this.rect.setX(i);
-                //  this.rect.setY(i); // если нужно с y
-                try {
-                    Thread.sleep(38);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-
         }
     }
 }
