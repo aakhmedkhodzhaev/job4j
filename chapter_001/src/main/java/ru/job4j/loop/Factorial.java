fb0bc8efcf1618e72924bcb3b10b0c645785407b
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-01-10 23:12:50 +0500

    5.1. Подсчет суммы чётных чисел в диапазоне Update 1.0 [#207097]

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,24 +1,25 @@
 package ru.job4j.loop;
 
+/**
+ * @task 5.2. Создать программу, вычисляющую факториал.[#207098]
+ * @aim Демонстрация Работы цикла for и возврат значения result
+ * @version 1.0 10.01.2020
+ * @author Akhmedkhodzhaev A.A.
+ * @others так же есть тест, для проверки работы программы
+ */
+
 public class Factorial {
     public int calc(int n){
         int result=0;
+        for(int i=0; i<=n; i++)
+        {
+            if(result==0 || i==0){
+                result=1;
+            }
+            else {
+                result = result * i;
+        }
+     }
         return result;
-    }
-
-    public static void main(String[]args){
-      int x=0;
-      for(int i=0; i<=4; i++)
-      {
-        if(x==0 || i==0){
-          x=1;
-        }
-        else {
-            x = x * i;
-        }
-
-      }
-        System.out.println(x);
-
     }
 }
