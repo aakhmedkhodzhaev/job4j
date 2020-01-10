1c069e53e9a3031157e0172b549e72a033c61db5
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-01-10 22:33:37 +0500

    5.1. Подсчет суммы чётных чисел в диапазоне[#207097]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+/**
+ * @task 6.1. Подсчет суммы чётных чисел в диапазоне[#207097]
+ * @aim Демонстрация Работы цикла for и возврат значения sum
+ * @version 1.0 10.01.2020
+ * @author Akhmedkhodzhaev A.A.
+ * @others так же есть тест, для проверки работы программы
+ */
+
+public class Counter {
+            public int add(int start, int finish){
+            int sum=0;
+                for(int i=start; i<=finish; i++){
+                if(i%2==0) {
+                    sum += i;
+                }
+                i++;
+              }
+              return sum;
+            }
+        }
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        Counter count = new Counter();
+        int start  = 0;
+        int finish = 10;
+        int sum = count.add(start,finish);
+        assertThat(sum, is(30)); //Проврека метода counter.add при диапазоне 0..10 сумма чётных чисел будет равна 30.
+    }
+}
