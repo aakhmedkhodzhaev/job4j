1021d176a57611cfd8051b9598f0220ef1c76574
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-01-12 19:31:17 +0500

    5.5. Простое число.[#207095]

---
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+/**
+ * @author Akhmedkhodzhaev A.A.
+ * @version 1.0 12.01.2020
+ * @task 5.5. Простое число.[#207095]
+ * @aim Демонстрация Работы цикла for и проверка числа простое или составное
+ * @others так же есть тест, для проверки работы программы
+ */
+
+public class CheckPrimeNumber {
+    public static boolean check(int finish){
+        boolean prime=true;
+        for(int i = 2; i<finish; i++){
+            if((finish%i)==0){
+                prime=false;
+            }
+        }
+        return prime;
+    }
+
+}
