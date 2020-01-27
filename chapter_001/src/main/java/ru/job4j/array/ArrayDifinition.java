985ac7e2ceda6e9cd2d2eba18cb91e76410ddfef
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-01-27 22:20:04 +0500

    6. Определение Массива.[#207048]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDifinition.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+/**
+ * @task 6. Определение Массива.[#207048]
+ * @aim Рассмотреть примеры создания массива
+ * @version 1.0 27.01.2020
+ * @author Akhmedkhodzhaev A.A.
+ * @others использовать разные типы данных
+ */
+
+public class ArrayDifinition {
+    public static void main(String[]args){
+        short [] ages = new short[10];
+        String [] surnames = new String[100500];
+        float [] prices = new float[40];
+    }
+}
