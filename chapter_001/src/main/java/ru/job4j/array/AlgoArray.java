5b2c037947c52a197a66c23f34120345a60353a9
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-02-04 23:47:32 +0500

    6.2. Перевернуть массив. Update 2.0 [#207050]

--- chapter_001/src/main/java/ru/job4j/array/AlgoArray.java
+++ chapter_001/src/main/java/ru/job4j/array/AlgoArray.java
@@ -11,24 +11,7 @@ package ru.job4j.array;
 public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[]{5, 3, 2, 1, 4};
-        int temp = array[0]; // переменная для временного хранения значение ячейки с индексом 0.
-        array[0] = array[3]; // записываем в ячейку с индексом 0 значение ячейки с индексом 3.
-        array[3] = temp; // записываем в ячейку с индексом 3 значение временной переменной.
-        temp = array[1];
-        array[1]=array[2];
-        array[2]=temp;
-        temp=array[3];
-        array[3]=array[4];
-        array[4]=temp;
-        for (int index = 0; index < array.length; index++) {
-            System.out.println(array[index]);
-        }
-    }
-}
-
-
-
-        /*   for (int i=0; i<array.length; i++) {
+        for (int i=0; i<array.length; i++) {
                for(int j=0; j<array.length-1; j++){ // метод пузырьком
                 if(array[j]>array[j+1]){
                    int temp = array[j];
@@ -39,7 +22,9 @@ public class AlgoArray {
        }
         for(int i=0; i<array.length; i++){
         System.out.println(array[i]);
-       }*/
+       }
+    }
+}
 
           /*int min = array[i];     // методом выбора
             int min_i = i;
