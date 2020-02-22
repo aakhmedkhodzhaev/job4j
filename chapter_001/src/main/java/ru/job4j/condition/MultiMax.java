efee02495584de2398c0607591f7443671ff3928
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-02-23 01:05:39 +0500

    4.4. Максимум из трех чисел[#207031]

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+/**
+ * @author Akhmedkhodzhaev A.A.
+ * @version 1.0 23.02.2020
+ * @task 4.4. Максимум из трех чисел[#207031]
+ * @aim научиться использовать тернарный оператор
+ * @others так же есть теста
+ */
+
+public class MultiMax {
+    public int max(int first, int second, int third) {
+        int result = (((first>=second) ? first:second)>=third) ? ((first>=second) ? first:second):third;
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MultiMaxTest {
+    @Test
+    public void whenSecondMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenFirstMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(7, 2, 6);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenThirdMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(2, 1, 5);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenDraw() {
+        MultiMax check = new MultiMax();
+        int result = check.max(11, 11, 11);
+        assertThat(result, is(11));
+    }
+}
