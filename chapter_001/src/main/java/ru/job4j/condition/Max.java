17b26fa635b8908795518c95c5fee5cf2c729efd
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-02-23 00:46:46 +0500

    4.2. Максимум из двух чисел[#207030]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+/**
+ * @author Akhmedkhodzhaev A.A.
+ * @version 1.0 23.02.2020
+ * @task 4.2. Максимум из двух чисел[#207030]
+ * @aim научиться использовать тернарный оператор
+ * @others так же есть теста
+ */
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = (left>=right )? left: right;
+        return result;
+    }
+
+}
