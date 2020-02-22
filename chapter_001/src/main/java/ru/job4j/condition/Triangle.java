fb95ebe046252ba41e140c4019a6c457cafaebad
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-02-23 00:55:20 +0500

    4.3. А существует ли треугольник?[#207033]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+/**
+ * @author Akhmedkhodzhaev A.A.
+ * @version 1.0 23.02.2020
+ * @task 4.3. А существует ли треугольник?[#207033]
+ * @aim научиться использовать тернарный оператор
+ * @others так же есть теста
+ */
+
+public class Triangle {
+    public static boolean exist(double ab, double ac, double bc) {
+        boolean result;
+        result = (ab + ac > bc && ac + bc > ab && ab + bc > ac) ? true : false;
+        return result;
+    }
+}
