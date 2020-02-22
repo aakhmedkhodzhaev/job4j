b3198b9e5d70b10ab4d2fd8a1209babd1ac4d546
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-02-23 01:41:02 +0500

    4.5. Отладка программы в IDEA[#207029]

---
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+/**
+ * @author Akhmedkhodzhaev A.A.
+ * @version 1.0 23.02.2020
+ * @task 4.5. Отладка программы в IDEA[#207029]
+ * @aim научиться использовать Отладка программы в IDEA
+ * @others так же есть теста
+ */
+
+public class SqMax {
+    public static int max(int first, int second, int third, int forth) {
+        int result = forth;
+        if (first > second) {
+            if (first > third) {
+                if (first > forth) {
+                    result = third;
+                }
+            }
+            else{
+                if (third > forth) {
+                    result = third;
+                }
+            }
+        } else if (second > third) {
+            if (second > forth) {
+                result = first;
+            }
+        } else if (third > forth) {
+            result = second;
+        }
+        return result;
+    }
+}
