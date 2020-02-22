05b41c2784e0591810f3852a2ffb77b78ddcc867
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-02-23 00:48:18 +0500

    4.0. Операторы ветвлений[#207032]

--- chapter_001/src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ chapter_001/src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 /**
  * @author Akhmedkhodzhaev A.A.
  * @version 1.0 23.02.2020
- * @task 4.0. Операторы ветвлений [#207032]
+ * @task 4.0. Операторы ветвлений[#207032]
  * @aim Операторы ветвлений
  * @others так же возможно написание теста
  */
